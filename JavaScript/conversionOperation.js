c12ae875389edd2fe1d6f63f7527e86059efb642
Prateek <Prateekrajgrd74@gmail.com>
2024-05-31 23:21:05 +0530

    31may.js

---
+++ JavaScript/conversionOperation.js
@@ -0,0 +1,19 @@
+let score = "33abc"
+console.log(typeof(score))
+
+let valueInNumber = Number(score)
+console.log(typeof(valueInNumber))
+console.log(valueInNumber)
+
+let isLoggedIn = 1
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn)
+
+// 1 => true; 0 => false
+// "" => false
+// "abc" => true
+
+let n = 33
+let stringNum = String(n)
+console.log(stringNum)
+console.log(typeof(stringNum))
