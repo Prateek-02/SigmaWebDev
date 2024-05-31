4ef94f2a9daa322c9f738976242d20b1480b55e4
Prateek <Prateekrajgrd74@gmail.com>
2024-05-31 23:05:45 +0530

    31may.js

---
+++ JavaScript/Variables.js
@@ -0,0 +1,15 @@
+const accountId = 14217384      // fixed value
+let accountEmail = "Prateek@gmail.com"
+var accountPassword = "12345"
+accountCity = "Jaipur"
+let accountState;
+
+// accountId = 2    (Not allowed)
+
+
+
+accountEmail = "Pr@abc.com"
+accountPassword = "13423"
+accountCity = "Delhi"
+console.log(accountId)
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState])
---
+++ JavaScript/dataTypes.js
@@ -0,0 +1,25 @@
+"use strict"; // treat all JS codes as newer version
+
+// alert("Hello")  // we are using nodejs, not browser
+
+let name = "prateek"
+let age = 20
+let isLoggedIn = true
+let state = null
+
+
+// number => 2^53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => value not assigned
+// symbol => uniqueness
+
+
+// object
+
+console.log(typeof "prateek")
+console.log(typeof age)
+console.log( typeof null)
+console.log( typeof undefined)
