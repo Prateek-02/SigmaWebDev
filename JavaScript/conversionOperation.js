fc160b4268d3dcd144f3a046d68a0e73ea4ca730
Prateek <Prateekrajgrd74@gmail.com>
2024-06-01 21:24:37 +0530

    1june.js

--- JavaScript/conversionOperation.js
+++ JavaScript/conversionOperation.js
@@ -17,3 +17,27 @@ let n = 33
 let stringNum = String(n)
 console.log(stringNum)
 console.log(typeof(stringNum))
+
+// ********** Operations *********
+
+let value = 3 
+let negvalue = -value
+console.log(negvalue)
+
+
+let str1 = "hello"
+let str2 = " Prateek"
+let str3 = str1+str2
+console.log(str3)
+
+// console.log("1"+2)
+// console.log(1+"2")
+// console.log("1"+2+2)
+// console.log(1+2+"2")
+// console.log((3+4) * 5 % 3)
+
+console.log(+true)
+
+let gameCounter = 100
+gameCounter++
+console.log(gameCounter)
