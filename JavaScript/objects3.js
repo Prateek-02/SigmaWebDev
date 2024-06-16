7aaca500790bd7a8509360b0e1a7bc885a6c90cb
Prateek <Prateekrajgrd74@gmail.com>
2024-06-16 20:30:08 +0530

    16thjune.js

---
+++ JavaScript/objects3.js
@@ -0,0 +1,26 @@
+const course = {
+    courseName: "Js in hindi",
+    price: "999",
+    courseInstructor: "Hitesh"
+}
+
+// course.courseInstructor
+
+const {courseInstructor: instructor} = course
+
+// console.log(courseInstructor);
+console.log(instructor);
+
+// API concept
+
+// {
+//     "name":"Prateek",
+//     "coursename": "js in hindi",
+//     "price": "free"
+// }
+
+[
+    {},
+    {},
+    {}
+]
