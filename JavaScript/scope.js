3424bd5cec6d09d62bba8ba6f24ab9fef83fe76f
Prateek <Prateekrajgrd74@gmail.com>
2024-06-16 23:42:53 +0530

    16thjune.js

---
+++ JavaScript/scope.js
@@ -0,0 +1,47 @@
+// var c = 300;
+
+let a = 300;
+
+if(true){
+    let a = 10;
+    const b = 20;  
+    var c = 30;    // var is global
+    // console.log("Inner: "+a);
+}
+
+
+
+// console.log(a);
+// console.log(b);
+// console.log(c);
+
+function one(){
+    const username = "prateek"
+
+    function Two(){
+        const website = "youtube"
+        console.log(`${username}`);
+    }
+    // console.log(website);
+    Two();
+}
+// one();
+
+if(true){
+    const username = "prateek";
+    if(username === "prateek"){
+        const website = "youtube"
+        // console.log(username+website)
+    }
+    // console.log(website);
+}
+// console.log(username);
+
+// +++++++++++++++ interesting  +++++++++++++++++++++
+
+function addOne(num){
+    return num+1;
+}
+
+let res = addOne(5);
+console.log(res);
