ec9b1af5aaab70381dd2d0eb21f326e8e01696bd
Prateek <Prateekrajgrd74@gmail.com>
2024-06-30 23:17:56 +0530

    dowhile.js

---
+++ JavaScript/tempCodeRunnerFile.js
@@ -0,0 +1,5 @@
+let i = 0;
+// while (i<=10) {
+//     console.log(`Value of index is ${i}`);
+//     i+=2;
+// }
---
+++ JavaScript/whileLoop.js
@@ -0,0 +1,29 @@
+// While Loop
+
+/*
+let i = 0;
+while (i<=10) {
+    console.log(`Value of index is ${i}`);
+    i+=2;
+}
+*/
+
+
+
+/*
+let myArr = ["IronMan","Thor","Hulk"];
+let arr = 0;
+
+while (arr < myArr.length) {
+    console.log(`Value is ${myArr[arr]}`);
+    arr++;
+}
+*/
+
+
+// do While Loop
+let score = 1;
+do {
+    console.log(`Score is ${score}`);
+    score++;
+} while (score<=10);
