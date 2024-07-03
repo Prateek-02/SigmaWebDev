d9d0ef0fa8cde6106c805c6c12792f02ef757fdf
Prateek <Prateekrajgrd74@gmail.com>
2024-07-03 16:29:31 +0530

    3rdjuly.js

--- JavaScript/map_filter_reduce.js
+++ JavaScript/map_filter_reduce.js
@@ -17,7 +17,7 @@ const myNums = [1,2,3,4,5,6,7,8,9,10];
 
 // myNums.forEach( (num) => {
 //     if(num > 4){
-//         newNums.push(num);n'
+//         newNums.push(num);
 //     }
 // });
 
@@ -61,3 +61,27 @@ console.log(userBooks);
 */
 
 
+// Map
+
+/*
+const myNums = [1,2,3,4,5,6,7,8,9,10];
+
+// const newNums = myNums.map( (num) => num+10);
+
+const newNums = myNums.map( (num) => num *10 ).map( (num) => num +1).filter( (num) => num >=40 );  // chaining
+console.log(newNums);
+*/
+
+
+// Reduce
+
+const myNums = [1,2,3];
+
+// const myTotal = myNums.reduce(function (acc,currval) {
+//     console.log(`acc: ${acc} and currval: ${currval}`);
+//     return acc + currval;
+// }, 0);
+
+const myTotal = myNums.reduce( (acc,curr) => acc + curr,0)
+
+console.log(myTotal);
