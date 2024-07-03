c1035602d5ecc397c4bec38477ab9fa807cc6a29
Prateek <Prateekrajgrd74@gmail.com>
2024-07-03 16:33:12 +0530

    3rdjuly.js

--- JavaScript/map_filter_reduce.js
+++ JavaScript/map_filter_reduce.js
@@ -85,3 +85,26 @@ const myNums = [1,2,3];
 const myTotal = myNums.reduce( (acc,curr) => acc + curr,0)
 
 console.log(myTotal);
+
+
+const shoppingCart = [
+    {
+        itemName : "js course",
+        price : 2999
+    },
+    {
+        itemName : "py course",
+        price : 3999
+    },
+    {
+        itemName : "data science course",
+        price : 12999
+    },
+    {
+        itemName : "mobile dev course",
+        price : 5999
+    },
+]
+
+const PriceToPay = shoppingCart.reduce( (acc,item) => acc + item.price,0);
+console.log(PriceToPay);
