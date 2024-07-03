cffbb46f0ea41c4a4b46396bcce131959c563168
Prateek <Prateekrajgrd74@gmail.com>
2024-07-03 16:10:07 +0530

    3rdjuly.js

--- JavaScript/forLoop.js
+++ JavaScript/forLoop.js
@@ -73,21 +73,59 @@ console.log(newArr)
 
 // Filter
 const greaterThanseven = (e) =>{
-    if(e>7){
-        return true
-    }
-    return false
+    return e > 7;
 }
 console.log(arr.filter(greaterThanseven))
 
 
 const myNums = [1,2,3,4,5,6,7,8,9,10];
-const newNums = myNums.filter( (e) => {
-    return e > 4;
-})
+// const newNums = myNums.filter( (e) => {
+//     return e > 4;
+// })
+// console.log(newNums);
 
-console.log(newNums);
+// const newNums = [];
 
+// myNums.forEach( (num) => {
+//     if(num > 4){
+//         newNums.push(num);n'
+//     }
+// });
+
+// console.log(newNums);
+
+const books = [
+    {
+        title : 'Book One', genere: 'Fiction', publish: 1981, edition : 2004
+    },
+    {
+        title : 'Book Two', genere: 'Non-Fiction', publish: 1992, edition : 2008
+    },
+    {
+        title : 'Book Three', genere: 'History', publish: 1999, edition : 2007
+    },
+    {
+        title : 'Book Four', genere: 'Non-Fiction', publish: 1989, edition : 2010
+    },
+    {
+        title : 'Book Five', genere: 'Fiction', publish: 2009, edition : 2014
+    },
+    {
+        title : 'Book Six', genere: 'Science', publish: 1987, edition : 2010
+    },
+    {
+        title : 'Book Seven', genere: 'Fiction', publish: 1986, edition : 1996
+    },
+    {
+        title : 'Book Eight', genere: 'History', publish: 2011, edition : 2016
+    }
+];
+
+
+let userBooks = books.filter( (bk) => bk.genere === 'History')
+
+userBooks = books.filter( (bk) => bk.publish >= 2000)
+console.log(userBooks);
 
 //Reduce
 
