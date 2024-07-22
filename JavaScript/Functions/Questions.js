599cb4666253e3dd451b65bdb75b47d958e238b6
Prateek <Prateekrajgrd74@gmail.com>
2024-07-22 06:48:50 +0530

    22ndjuly.js

--- JavaScript/Functions/Questions.js
+++ JavaScript/Functions/Questions.js
@@ -1,6 +1,6 @@
 // 1 (Count number of Vowels in the given string)
 
-
+/*
 function countVowels(str){
     str = str.toLowerCase();
     let count = 0;
@@ -10,10 +10,26 @@ function countVowels(str){
             count++;
         }
     }
-    console.log(`Numbes of vowels in the string are: ${count}`);
+    return(`Numbes of vowels in the string are: ${count}`);
 }
 
-countVowels("ApnaCollege");
+const res = countVowels("ApnaCollege");
+console.log(res);
+*/
 
+// Using Arrow function
 
+const countVowels = (str) => {
+    str = str.toLowerCase();
+    let count = 0;
+    for(const char of str){
+        // console.log(char);
+        if(char === 'a' || char === 'e' || char === 'i' || char === 'o' || char === 'u' ){
+            count++;
+        }
+    }
+    return(`Numbes of vowels in the string are: ${count}`);
+}
 
+const res = countVowels("ApnaCollege");
+console.log(res);
