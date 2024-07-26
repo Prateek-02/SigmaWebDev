ac28ef7833af5983eb8a2650acea99ad43ebc4bf
Prateek <Prateekrajgrd74@gmail.com>
2024-07-26 18:26:13 +0530

    26thjuly.js

--- JavaScript/Objects&Classes/Questions.js
+++ JavaScript/Objects&Classes/Questions.js
@@ -1,3 +1,6 @@
+// 1. create a user class with two properties name and email.It also ahs a method called viewData() that allows user to view the website data
+
+/*
 let DATA = "Secret Information";
 
 class User{
@@ -25,3 +28,35 @@ student1.viewData();
 student2.viewData();
 
 teacher1.viewData();
+*/
+
+
+// 2 crate a new class called Admin which inherits from user. Add a new method called editData to Admin that allows it to edit website data.
+
+let DATA = "Secret Information";
+
+class User{
+    constructor(name,email){
+        this.name = name;
+        this.email = email;
+    }
+
+    viewData(){
+        console.log("Data = ",DATA);
+    }
+}
+
+class Admin extends User{
+    constructor(name,email){
+        super(name,email);
+    }
+
+    editData(){
+        DATA = "Secret Information updated";
+    }
+}
+
+let admin1 = new Admin("admin","admin@college.com");
+
+admin1.editData();
+admin1.viewData();
