54e4da1dd94efe4f7037bb5ae55ff216e08222b9
Prateek <Prateekrajgrd74@gmail.com>
2024-10-08 21:12:02 +0530

    counter.js

---
+++ ReactJs/02counter/src/App.jsx
@@ -0,0 +1,43 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+  let [count ,setCount] = useState(0);
+
+  const addValue = () =>{
+    if(count >= 20){
+      count = 0;
+    } 
+    else{
+      setCount(count+1);
+    }
+  }
+
+  const remValue = () =>{
+    if(count <= 0){
+      count = 0;
+    }
+    else{
+      setCount(count - 1);
+    }
+    
+  }
+
+  return (
+    <>
+      <h1>Chai aur React</h1>
+      <h2>Counter value: {count}</h2>
+
+      {/* <button onClick={() => {setCount(count+1)}}>Add Value </button> */}
+      <button onClick={addValue}>Add Value </button>
+      <br /><br/>
+      {/* <button onClick={() => {setCount(count-1)}}>Remove Value</button> */}
+      <button onClick={remValue}>Remove Value</button>
+
+
+    </>
+  )
+}
+
+export default App
