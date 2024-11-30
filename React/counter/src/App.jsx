aa9d9b081f1d950b7fd1d4bbd7e8e3d6e1398a0c
Prateek <Prateekrajgrd74@gmail.com>
2024-12-01 00:32:37 +0530

    a.js

---
+++ React/counter/src/App.jsx
@@ -0,0 +1,25 @@
+import { useState } from 'react';
+import './App.css'
+
+function App() {
+  
+  const[counter,setCount] = useState(0);
+  if(counter < 0){
+    setCount(0);
+  }
+  if(counter>20){
+    setCount(0);
+  }
+
+  return (
+    <>
+      <h1>Chai Aur react</h1>
+      <h2>Counter Value: {counter}</h2>
+      <button onClick={() => setCount(counter+1)}>Add Value</button>
+      <br /><br />
+      <button onClick={() => setCount(counter-1)}>Sub Value</button>
+    </>
+  )
+}
+
+export default App
