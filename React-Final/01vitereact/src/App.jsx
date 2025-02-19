15d71138444266a75e882d3de274201dcda12540
Prateek <Prateekrajgrd74@gmail.com>
2025-02-20 00:33:24 +0530

    react

--- React-Final/01vitereact/src/App.jsx
+++ React-Final/01vitereact/src/App.jsx
@@ -1,7 +1,13 @@
+import Chai from "./Chai"
+
 function App() {
 
   return (
-    <h1>Chai aur React with Vite | Prateek</h1>
+    <>
+      <h1>Chai aur React with Vite | Prateek</h1>
+      <Chai/>
+    </>
+    
   )
 }
 
