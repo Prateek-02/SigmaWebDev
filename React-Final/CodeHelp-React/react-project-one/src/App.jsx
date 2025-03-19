c1865d2f39940dd275df29b8744ac76d11ced5b2
Prateek <Prateekrajgrd74@gmail.com>
2025-03-19 12:23:01 +0530

    react-final

---
+++ React-Final/CodeHelp-React/react-project-one/src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+import Navbar from './components/Navbar'
+
+function App() {
+
+
+  return (
+    <>
+      <Navbar/>
+      <p>Hello, My name is Prateek Raj</p>
+    </>
+  )
+}
+
+export default App
---
+++ React-Final/CodeHelp-React/react-project-one/src/components/Navbar.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Navbar() {
+  return (
+    <div>
+      <div>Mai Navbar hu</div>
+    </div>
+  )
+}
+
+export default Navbar
